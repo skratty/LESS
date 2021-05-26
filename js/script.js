9d5b4f130f8c4d1c97e2775d567a14f65916fdc6
Olya <44779117+skratty@users.noreply.github.com>
2021-05-26 16:43:04 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+	var Theme_number = 1;
+	var Default_theme = 1;
+	var Ext = "less";
+    //var Ext = "css";
+
+	function getUrl(num) {
+		return "css" + "/" + Ext + ".style" + num + ".css";
+    }
+
+    function getThemeNumber() {
+		Theme_number = Theme_number % 2 === 0 ? 1 : 2;
+		console.log(Theme_number);
+		return Theme_number;
+    }
+
+    function initialize() {
+		var link = document.createElement("link");
+		link.setAttribute("rel", "stylesheet");
+		link.setAttribute("href", getUrl(Default_theme));
+		document.head.appendChild(link);
+    }
+
+    function initListener() {
+		document
+			.querySelector(".mynav__theme")
+		    .addEventListener("click", changeTheme);
+	}
+
+	function changeTheme() {
+        document.head
+            .querySelector("link")
+            .setAttribute("href", getUrl(getThemeNumber()));
+    }
+
+    initialize();
+
+    window.addEventListener("load", initListener);
+
